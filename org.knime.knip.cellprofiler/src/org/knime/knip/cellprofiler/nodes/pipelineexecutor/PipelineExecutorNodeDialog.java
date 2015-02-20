94604a133cd060f6bbcc923a8575f326856f88f2
dietzc <christian.dietz@uni-konstanz.de>
2015-02-20 15:48:17 +0100

    dialog: organize imports

--- org.knime.knip.cellprofiler/src/org/knime/knip/cellprofiler/nodes/pipelineexecutor/PipelineExecutorNodeDialog.java
+++ org.knime.knip.cellprofiler/src/org/knime/knip/cellprofiler/nodes/pipelineexecutor/PipelineExecutorNodeDialog.java
@@ -10,7 +10,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import javax.swing.BorderFactory;
